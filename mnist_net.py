a8bcf40189f2815309ffcecaa8909ee46b082227
ChiaraRgnz <64198751+ChiaraRgnz@users.noreply.github.com>
2021-06-13 19:56:26 +0200

    Update mnist_net.py

--- mnist_net.py
+++ mnist_net.py
@@ -7,7 +7,7 @@
 import torch
 import torch.nn as nn
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Flatten(nn.Module):
     def forward(self, x):
