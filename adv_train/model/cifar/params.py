16944e51ccbb73f4a414c27792ca405760f919ba
Hugo Berard <hberard@fb.com>
2021-06-17 09:47:39 -0400

    Adding code from Online Attacks project to easily load different models

---
+++ adv_train/model/cifar/params.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from omegaconf import MISSING
+from .models import CifarModel
+from online_attacks.classifiers.dataset import DatasetParams
+from online_attacks.attacks import Attacker, AttackerParams
+from online_attacks.utils.optimizer import OptimizerParams, OptimizerType
+from typing import Optional
+
+
+@dataclass
+class CifarTrainingParams:
+    name: str = "cifar"
+    model_type: CifarModel = MISSING
+    num_epochs: int = 100
+    optimizer_params: OptimizerParams = OptimizerParams(optimizer_type=OptimizerType.SLS)
+    dataset_params: DatasetParams = DatasetParams()
+    save_model: bool = True
+    save_dir: str = "./pretained_models"
+    train_on_test: bool = False
+    attacker: Attacker = Attacker.NONE
+    attacker_params: AttackerParams = AttackerParams()
+    model_dir: str = "./pretrained_models/"
+    model_attacker: Optional[str] = None
---
+++ adv_train/model/dataset.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from dataclasses import dataclass
+from omegaconf import MISSING
+
+
+class DatasetType(Enum):
+    MNIST = "mnist"
+    CIFAR = "cifar"
+
+
+@dataclass
+class DatasetParams:
+    data_dir: str = "./data"
+    batch_size: int = 256
+    test_batch_size: int = 512
+    num_workers: int = 4
+    shuffle: bool = True
+    download: bool = True
