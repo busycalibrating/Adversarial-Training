aa1ea06202a599f404f304755446846525ecda70
ChiaraRgnz <64198751+ChiaraRgnz@users.noreply.github.com>
2021-06-28 11:32:55 +0200

    Update train_adv.py

--- adv_train/scripts/train_adv.py
+++ adv_train/scripts/train_adv.py
@@ -37,8 +37,8 @@ class AdversarialTraining(Launcher):
         self.dataset = AdversarialDataset(dataset)
         self.dataloader = DataLoader(self.dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
 
-        self.model =  load_mnist_classifier(MnistModel.MODEL_A, device=self.device, eval=False)
-
+        #self.model =  load_mnist_classifier(MnistModel.MODEL_A, device=self.device, eval=False)
+        self.model =  load_mnist_classifier(MnistModel.MODEL_BBis, device=self.device, eval=False)
         self.model_eval = None
         if args.eval_name is not None:
             self.model_eval = load_mnist_classifier(MnistModel.MODEL_B, name=args.eval_name, model_dir=args.model_dir, device=self.device, eval=True)
